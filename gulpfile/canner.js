a420c38a675392fe913a4e4d62078be1cde450a3
JackHou <vjack070707@gmail.com>
2015-10-06 16:37:32 +0800

    up

--- gulpfile/canner.js
+++
@@ -1,9 +0,0 @@
-var canner = require('gulp-canner');
-var gulp = require('gulp');
-var config = require('./config');
-// var browserSync = require('browser-sync').create();
-
-gulp.task('canner', function() {
-    gulp.src(config.canner.files)
-        .pipe(canner());
-});
